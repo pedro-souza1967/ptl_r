076253e031be5c9fd3bf7274198f9783cb268db9
Pedro Carlos <pedrocarlosouza@gmail.com>
2022-08-21 16:42:41 -0300

    Script 1

---
+++ script 1.R
@@ -0,0 +1,18 @@
+# aprender comandos básicos no R
+# Utilizar o R como calculadora
+5+5
+5*2
+5*2
+5^2
+sqrt(25)
+# criar objetos
+a=5
+b=6
+a*b
+# criando vetores
+nomes=c("pedro","maria","paulo")
+notas=c(50,90,78)
+#Criando vetores
+
+data.frame(nomes,notas)
+     
